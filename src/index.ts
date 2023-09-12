56f2bac4e77007d010937fe631f6650064315b1c
esther_chen <esther_chen@trendmicro.com>
2023-09-12 13:09:48 +0800

    fix pr

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import { OnRpcRequestHandler, OnTransactionHandler } from '@metamask/snaps-types'
-import { panel, copyable, divider, heading, text } from '@metamask/snaps-ui'
-import { API, ENABLE_CLIENT_CONSOLE } from './constants/config'
-import Logger from './controllers/logger'
+import { OnTransactionHandler } from '@metamask/snaps-types'
 import { transactionInsightLayout } from './helpers/snapContent'
+import Logger from './controllers/logger'
 
 const logger = new Logger('[index]')
 export const onTransaction: OnTransactionHandler = async ({
@@ -15,39 +13,4 @@ export const onTransaction: OnTransactionHandler = async ({
   logger.log('transaction:', transaction)
 
   return transactionInsightLayout({ transactionOrigin, chainId, transaction })
-}
-
-/**
- * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
- *
- * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
- * @param args.request - A validated JSON-RPC request object.
- * @returns The result of `snap_dialog`.
- * @throws If the request method is not valid for this snap.
- */
-export const onRpcRequest: OnRpcRequestHandler = async ({ origin, request }) => {
-  switch (request.method) {
-    case 'hello':
-      return snap.request({
-        method: 'snap_dialog',
-        params: {
-          type: 'alert',
-          content: panel([
-            text(`Hello, **${origin}**!😑🫨😬`),
-            divider(),
-            heading('Envirment Variables'),
-            text(`${ENABLE_CLIENT_CONSOLE ? 'dev' : 'prod'} `),
-            text(`API PGW BASE: ${API.PGW.base}`),
-            divider(),
-            heading('Params From Dapp'),
-            copyable(`${origin}`),
-            text(`${JSON.stringify(request.params)}`),
-          ]),
-        },
-      })
-    default:
-      throw new Error('Method not found.')
-  }
 }
