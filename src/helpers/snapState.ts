9f7496aa7df5e27a8ef572b8c06b909d2f41188c
esther_chen <esther_chen@trendmicro.com>
2023-09-19 11:03:45 +0800

    MSS-234 add snap state management

---
+++ src/helpers/snapState.ts
@@ -0,0 +1,33 @@
+import { TGetSnapState, TSnapState } from './types/snapState.type'
+import Logger from '../controllers/logger'
+const looger = new Logger('[helpers.snapState]')
+
+export const getSnapState: TGetSnapState = async () => {
+  const state = await snap.request({
+    method: 'snap_manageState',
+    params: {
+      operation: 'get',
+    },
+  })
+  looger.log('get state:', state)
+  return state
+}
+
+export const setSnapState = async (newState: TSnapState) => {
+  looger.log('set state:', newState)
+  return snap.request({
+    method: 'snap_manageState',
+    params: {
+      operation: 'update',
+      newState: newState,
+    },
+  })
+}
+
+export const clearSnapState = async () => {
+  looger.log('clearSnapState:')
+  return snap.request({
+    method: 'snap_manageState',
+    params: { operation: 'clear' },
+  })
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { OnTransactionHandler } from '@metamask/snaps-types'
+import { OnTransactionHandler, OnRpcRequestHandler } from '@metamask/snaps-types'
 import { transactionInsightLayout } from './helpers/snapContent'
 import Logger from './controllers/logger'
+import { SnapInfo, TSnapState } from './helpers/types/snapState.type'
+import { getSnapState, setSnapState } from './helpers/snapState'
 
 const logger = new Logger('[index]')
 export const onTransaction: OnTransactionHandler = async ({
@@ -11,6 +13,22 @@ export const onTransaction: OnTransactionHandler = async ({
   logger.log('transactionOrigin:', transactionOrigin)
   logger.log('chainId:', chainId)
   logger.log('transaction:', transaction)
+  logger.log('snap state:', getSnapState())
 
   return transactionInsightLayout({ transactionOrigin, chainId, transaction })
+}
+
+export const onRpcRequest: OnRpcRequestHandler = async ({ origin, request }) => {
+  switch (request.method) {
+    case 'storeState':
+      if (request.params) {
+        const state: TSnapState = (request.params as TSnapState) ?? undefined
+        if (state) {
+          await setSnapState(state)
+        }
+      }
+      return true
+    default:
+      throw new Error('Method not found.')
+  }
 }
