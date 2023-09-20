7f8661d37050e7004a75f39090913268091c82b8
esther_chen <esther_chen@trendmicro.com>
2023-09-21 00:47:40 +0800

    hot fix

--- src/helpers/versionCheck.ts
+++ src/helpers/versionCheck.ts
@@ -1,10 +1,10 @@
 export const isGreaterVersion = (version: string, compareVersion: string) => {
-  const versionArray = version.split('.')
-  const compareVersionArray = compareVersion.split('.')
-  for (let i = 0; i < versionArray.length; i++) {
-    if (parseInt(versionArray[i]) < parseInt(compareVersionArray[i])) {
-      return false
+    const versionArray = version.split('.')
+    const compareVersionArray = compareVersion.split('.')
+    for (let i = 0; i < versionArray.length; i++) {
+        if (parseInt(versionArray[i]) > parseInt(compareVersionArray[i])) {
+            return true
+        }
     }
-  }
-  return true
+    return false
 }
